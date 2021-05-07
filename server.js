9d6a3018b1559747d22a908fe62ed0cd4b180d80
SanthoshKumarBethi <santhosh.bethi@wanbuffer.com>
2021-05-07 21:02:00 +0530

    db and new apis added

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ require('./app/routes/home.routes')(app);
 
 
 // set port, listen for requests
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
